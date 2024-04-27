5233876bb8fd9dca2430bc9914ffa29dc97a2f37
Mohamedamine Elazziz <Amineelazziz0@gmail.com>
2024-04-27 13:15:46 +0100

    addition de systeme de deconnection

--- Vendre_pieces_auto/Controllers/AuthoController.cs
+++ Vendre_pieces_auto/Controllers/AuthoController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Vendre_pieces_auto.Controllers
 {
@@ -14,5 +16,13 @@ namespace Vendre_pieces_auto.Controllers
 
             await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);//Cette ligne lance le processus d'authentification en utilisant le schéma d'authentification spécifié (dans ce cas, Auth0Constants.AuthenticationScheme). L'appel à ChallengeAsync demande à ASP.NET Core de lancer un flux d'authentification externe. Les propriétés d'authentification configurées sont passées en tant que paramètre.
         }
+        [Authorize]
+        public async Task<IActionResult> Logout()// C’est la déclaration de la méthode. Elle est publique, donc accessible en dehors de la classe. async signifie qu’elle est asynchrone, c’est-à-dire qu’elle peut être exécutée en parallèle avec d’autres tâches. Task<IActionResult> est le type de retour, qui indique qu’elle renvoie une tâche qui, lorsqu’elle est terminée, donnera un IActionResult, qui est une interface pour les types de résultats d’action.
+        {
+            await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme);//Cette ligne déconnecte l’utilisateur de la session Auth0. "HttpContext" est une propriété qui fournit des informations sur la requête HTTP en cours. "SignOutAsync" est une méthode qui termine la session de l’utilisateur pour le schéma d’authentification donné, dans ce cas "Auth0Constants.AuthenticationScheme".
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);//Cette ligne déconnecte l’utilisateur de la session d’authentification par cookies. C’est similaire à la ligne précédente, mais pour le schéma d’authentification par défaut des cookies.
+            return RedirectToAction("InterfaceUser", "User_Interface");//Enfin, cette ligne redirige l’utilisateur vers l’action "InterfaceUser" du contrôleur "User_Interface" après la déconnexion.
+        }
+
     }
 }
