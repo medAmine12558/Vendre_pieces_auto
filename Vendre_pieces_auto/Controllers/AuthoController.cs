82263ead1de0268d7bfbcf2f2943e408ddbe64b1
Mohamedamine Elazziz <Amineelazziz0@gmail.com>
2024-04-27 13:30:13 +0100

    ajout de processus de logout

--- Vendre_pieces_auto/Controllers/AuthoController.cs
+++ Vendre_pieces_auto/Controllers/AuthoController.cs
@@ -10,11 +10,11 @@ namespace Vendre_pieces_auto.Controllers
     {
         public async Task Login(string returnUrl = "/") //la fonction qui gere l'auth externe (avec google ou facbook) et l'auth avec email et password
         {
-            var authenticationProperties = new LoginAuthenticationPropertiesBuilder() //Cette ligne crée une nouvelle instance de LoginAuthenticationPropertiesBuilder, un constructeur pour construire des propriétés d'authentification pour l'authentification externe. Ces propriétés permettent de configurer le comportement de l'authentification.
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder() //Cette ligne crée une nouvelle instance de LoginAuthenticationPropertiesBuilder, un constructeur pour construire des propriétés d'authentification pour l'authentification externe. Ces propriétés permettent de configurer le comportement de l'authentification. d'une autre maniere il construirt un objet authenticationProperties qui contient des informations sur la manière dont le processus d’authentification doit se dérouler
                 .WithRedirectUri(returnUrl) //définit l'URL de redirection après l'authentification. Elle prend comme argument l'URL returnUrl fournie en paramètre de la méthode Login.
                 .Build(); // Cette méthode Build finalise la construction des propriétés d'authentification et retourne l'objet authenticationProperties configuré.
 
-            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);//Cette ligne lance le processus d'authentification en utilisant le schéma d'authentification spécifié (dans ce cas, Auth0Constants.AuthenticationScheme). L'appel à ChallengeAsync demande à ASP.NET Core de lancer un flux d'authentification externe. Les propriétés d'authentification configurées sont passées en tant que paramètre.
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);//Cette méthode déclenche le processus d’authentification. "Auth0Constants.AuthenticationScheme" est le nom du schéma d’authentification utilisé (dans ce cas, Auth0), et "authenticationProperties" est l’objet que nous avons construit précédemment.
         }
         [Authorize]
         public async Task<IActionResult> Logout()// C’est la déclaration de la méthode. Elle est publique, donc accessible en dehors de la classe. async signifie qu’elle est asynchrone, c’est-à-dire qu’elle peut être exécutée en parallèle avec d’autres tâches. Task<IActionResult> est le type de retour, qui indique qu’elle renvoie une tâche qui, lorsqu’elle est terminée, donnera un IActionResult, qui est une interface pour les types de résultats d’action.
