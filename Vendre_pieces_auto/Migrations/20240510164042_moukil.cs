9162878ea7de551e5f8724fc9bf2350d85a57b5e
Ayman Moukil <aymanesalahmoukil@gmail.com>
2024-05-10 17:52:59 +0100

    bien effectuer

---
+++ Vendre_pieces_auto/Migrations/20240510164042_moukil.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vendre_pieces_auto.Migrations
+{
+    /// <inheritdoc />
+    public partial class moukil : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
